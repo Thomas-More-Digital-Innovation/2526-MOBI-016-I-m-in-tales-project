da87bb4b071736470af034f7fd50a1d04001e8bc
StefanBotnariUni <r1033310@student.thomasmore.be>
2025-12-10 13:36:25 +0100

    Added the initial form to create a new story + uploading image and json

---
+++ code/src/app/components/StoryBuilder.tsx
@@ -0,0 +1,14 @@
+export default function storyBuilder() {
+    return(
+        <div className="flex">
+            {/* Node Connections/Selection */}
+            <div>
+                
+            </div>
+            {/* Form */}
+            <div>
+
+            </div>
+        </div>
+    )
+}
--- code/src/app/makeStory/storyConfigurator/page.tsx
+++ code/src/app/makeStory/storyConfigurator/page.tsx
@@ -1,12 +1,12 @@
-import { Header, StageNode } from "@components"
-import { useParams } from "react-router-dom"
+import { Header, StageNode } from "@components";
+import { useParams } from "react-router-dom";
 export default function storyConfigurator() {
     const { folderName } = useParams();
-    
+
     return (
         <main className="flex flex-col">
             <Header />
-            <StageNode folderName={folderName}/>
+            <StageNode folderName={folderName} />
         </main>
-    )
+    );
 }
