b46eba093aadfa6d0f18df39882afdca7e733c9f
Sem Van Broekhoven <144097969+dotsem@users.noreply.github.com>
2026-01-20 08:39:39 +0100

    change a tags to Links for better routing

--- code/src/app/components/LargerButton.tsx
+++ code/src/app/components/LargerButton.tsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 export default function LargerButton({
   label,
   link = "#",
@@ -8,12 +10,12 @@ export default function LargerButton({
   imageLink: string;
 }) {
   return (
-    <a
-      href={link}
+    <Link
+      to={link}
       className="inline-flex flex-col items-center text-center gap-4 p-[5vw] border-2 rounded-2xl border-talesblu-400 hover:shadow-2xl hover:bg-talesorang-400 hover:text-white hover:border-white ease-in-out duration-300"
     >
       <img src={imageLink} alt="Image Icon" width={100} height={100} />
       <span className="text-2xl font-semibold">{label}</span>
-    </a>
+    </Link>
   );
 }
